01b42365ff727467d09ca0347c8f3ac1f1b00293
nttejun <wonjun.jeong@buxikorea.com>
2020-06-04 21:48:18 +0900

    Added basic project setting

--- board/src/main/java/com/wonjun/board/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wonjun.board.repository;
-
-import com.wonjun.board.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
